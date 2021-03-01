b54c2f25073d4c2dc49b16ee47a62cf67d68723e
Pusha <de24world@gmail.com>
2021-03-01 21:18:26 +0100

    add showhide State 4:26 초까지

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function Word({ word }) {
+  const [isShow, setIsShow] = useState(false);
+  function toggleShow() {
+    setIsShow(!isShow);
+  }
+
   return (
     <tr>
       <td>{word.eng}</td>
-      <td>{word.kor}</td>
+      <td>{isShow && word.kor}</td>
       <td>
-        <button> 뜻보기 </button>
+        <button onClick={toggleShow}>
+          뜻보기 {isShow ? "숨기기" : "보기"}{" "}
+        </button>
         <button class="btn_del">삭제</button>
       </td>
     </tr>
