45e73cb0b780223d1a0d211046d10df1849f4ab0
Pusha <de24world@gmail.com>
2021-03-02 20:02:14 +0100

    add toggleDone

--- src/component/Word.js
+++ src/component/Word.js
@@ -2,12 +2,21 @@ import { useState } from "react";
 
 export default function Word({ word }) {
   const [isShow, setIsShow] = useState(false);
+  const [isDone, setIsDone] = useState(word.isDone);
+
   function toggleShow() {
     setIsShow(!isShow);
   }
 
+  function toggleDone() {
+    setIsDone(!isShow);
+  }
+
   return (
-    <tr>
+    <tr className={isDone ? "off" : ""}>
+      <td>
+        <input type="checkbox" checked={isDone} onChange={toggleDone} />
+      </td>
       <td>{word.eng}</td>
       <td>{isShow && word.kor}</td>
       <td>
