73bfa03f825980fa47ab1f06b4b1de4487911321
Pusha <de24world@gmail.com>
2021-03-06 20:14:03 +0100

    PUT Update

--- src/component/Word.js
+++ src/component/Word.js
@@ -9,7 +9,21 @@ export default function Word({ word }) {
   }
 
   function toggleDone() {
-    setIsDone(!isShow);
+    // setIsDone(!isShow);
+    fetch("http://localhost:3001/words/${word.id", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone
+      })
+    }).then((res) => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
   }
 
   return (
